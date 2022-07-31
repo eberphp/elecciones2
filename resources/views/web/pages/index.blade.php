8718b1e30166d2e78c2647a456eb61f8fdf3164a
eberphp <erb159@gmail.com>
2022-07-31 16:21:48 -0500

    31/07/2022 16.21

--- resources/views/web/pages/index.blade.php
+++ resources/views/web/pages/index.blade.php
@@ -351,7 +351,7 @@
                                     <div class="swiper-wrapper altutra">
                                         @foreach ($imagenes as $imagen)
                                         <div class="swiper-slide new-alturita-bloque-3" style="">
-                                            <div class="bg-img valign" style="background-size: 100%!important;background-position: center!important;" data-background="{{ asset('img/publicaciones/galeria/'.$imagen->imagen)}}" data-overlay-dark="6">
+                                            <div class="bg-img valign" style="background-size: 100%!important;" data-background="{{ asset('img/publicaciones/galeria/'.$imagen->imagen)}}" data-overlay-dark="6">
                                                 
                                             </div>
                                         </div>
