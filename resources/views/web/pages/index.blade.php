e7bd5b32bd6406144e51533c93ccb0d2e1d7bf89
eberphp <erb159@gmail.com>
2022-07-31 18:36:28 -0500

    31/07/2022 18.36

--- resources/views/web/pages/index.blade.php
+++ resources/views/web/pages/index.blade.php
@@ -97,7 +97,7 @@
     <section class="services bords section-padding pt-10" style="padding-bottom: 0%;">
         <div class="container" style="max-width: 100%;">
             <div class="row">
-                @if ($titulo->tituloServicioVisible == 'SI')
+                {{--@if ($titulo->tituloServicioVisible == 'SI')
                 <div class="col-lg-6 wow fadeInLeft" data-wow-delay=".5s">
                     <div class="form-group has-error has-danger controls">
                         <select name="" id="" class="form-control form-group">
@@ -113,10 +113,10 @@
                         </select>
                     </div>
                 </div>
-                @endif
+                @endif--}}
     
                 @if ($titulo->tituloServicioVisible == 'SI')
-                <div class="col-lg-6 wow fadeInLeft" data-wow-delay=".7s">
+                <div class="col-lg-12 wow fadeInLeft" data-wow-delay=".7s">
                     <div class="form-group has-error has-danger controls">
                         <select name="" id="" class="form-control form-group" onchange="redireciona()">
                             <option value="null">{{$titulo->titleServicio}}</option>
