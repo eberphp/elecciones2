05031d4c3bf4716e62e46d24751c5baa61f582dd
angelgonzalezacevedo12@outlook.com <angelgonzalezacevedo12@outlook.com>
2022-08-01 13:59:13 -0500

    asd

--- app/Models/vinculo.php
+++ app/Models/vinculo.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class vinculo extends Model
+class Vinculo extends Model
 {
     public $table = 'vinculos';
     use HasFactory;
