b0fbe7e7cb24f78ffdc448d0a9f957d9bb0d6fc5
EnriqueVargasSoto <45204929+EnriqueVargasSoto@users.noreply.github.com>
2023-03-10 11:56:51 -0500

    10/03/2023 11.56

--- app/Http/Controllers/Empresa/DatosController.php
+++ app/Http/Controllers/Empresa/DatosController.php
@@ -31,7 +31,7 @@ class DatosController extends Controller
             $imagen = $request->file("favicon");
             $nombreimagenFavicon = Str::slug($imagen->getClientOriginalName() . microtime()) . "." . $imagen->guessExtension();
             $rutasave = "public/img/favicon/";
-            $path = Storage::disk('public')->putFileAs($rutasave, $imagen, $nombreimagenFavicon);
+            $path = Storage::disk('public')->putFileAs($rutasave, $imagen, $nombreimagenFavicon, 'public');
         } else {
             $nombreimagenFavicon = $datos->favicon;
         }
